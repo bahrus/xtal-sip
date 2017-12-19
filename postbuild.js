02a6712787211a7ca9553a1db7b505b5e7294b43
bahrus <anderson.bruce.b@gmail.com>
2017-12-19 13:10:28 -0500

    xtal-sip

--- postbuild.js
+++ postbuild.js
@@ -1,4 +1,4 @@
-const fs = require('fs-extra')
+//const fs = require('fs-extra')
 // try{
 //     fs.copySync('build/ES5/src/v0/xtal-in.js', 'build/ES5/xtal-in.js')
 //     fs.copySync('build/ES6/src/v0/xtal-in.js', 'build/ES6/xtal-in.js')
