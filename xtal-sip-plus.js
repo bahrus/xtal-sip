ab46682ff1d55d75dc02d39f0dc39d9dd44436b2
bahrus <anderson.bruce.b@gmail.com>
2018-02-03 22:52:26 -0500

    add delay

--- xtal-sip-plus.js
+++ xtal-sip-plus.js
@@ -1,72 +1,82 @@
-do {
-    const XtalSip = customElements.get('xtal-sip');
-    function replace(str, find, replace) {
-        return str.replace(new RegExp(find, 'g'), replace);
+(function () {
+    if (document.readyState !== "loading") {
+        plus();
     }
-    const detail = {};
-    document.head.dispatchEvent(new CustomEvent('xtal-sip-init', {
-        detail: detail,
-    }));
-    const tB = detail['tieBreaker'];
-    const sub = detail['substitutor'];
-    const tagToFakeLink = {};
-    [].slice.call(document.head.querySelectorAll('link[rel-ish="preload"]')).forEach(el => {
-        if (sub)
-            sub(el); //substitution
-        el.dataset.tags.split(',').forEach(tag => {
-            if (!tagToFakeLink[tag])
-                tagToFakeLink[tag] = [];
-            tagToFakeLink[tag].push(el);
+    else {
+        document.addEventListener("DOMContentLoaded", e => {
+            plus();
         });
-    });
-    const goodFakeLinkEls = [];
-    //tie breaker
-    for (var key in tagToFakeLink) {
-        const els = tagToFakeLink[key];
-        let elToAdd;
-        if (els.length === 1) {
-            elToAdd = els[0];
+    }
+    function plus() {
+        const XtalSip = customElements.get('xtal-sip');
+        function replace(str, find, replace) {
+            return str.replace(new RegExp(find, 'g'), replace);
         }
-        else {
-            if (tB) {
-                elToAdd = tB(key, els);
+        const detail = {};
+        document.head.dispatchEvent(new CustomEvent('xtal-sip-init', {
+            detail: detail,
+        }));
+        const tB = detail['tieBreaker'];
+        const sub = detail['substitutor'];
+        const tagToFakeLink = {};
+        [].slice.call(document.head.querySelectorAll('link[rel-ish="preload"]')).forEach(el => {
+            if (sub)
+                sub(el); //substitution
+            el.dataset.tags.split(',').forEach(tag => {
+                if (!tagToFakeLink[tag])
+                    tagToFakeLink[tag] = [];
+                tagToFakeLink[tag].push(el);
+            });
+        });
+        const goodFakeLinkEls = [];
+        //tie breaker
+        for (var key in tagToFakeLink) {
+            const els = tagToFakeLink[key];
+            let elToAdd;
+            if (els.length === 1) {
+                elToAdd = els[0];
+            }
+            else {
+                if (tB) {
+                    elToAdd = tB(key, els);
+                }
+            }
+            if (elToAdd) {
+                if (elToAdd['_a'])
+                    continue; //already added
+                elToAdd['_a'] = true;
+                goodFakeLinkEls.push(elToAdd);
             }
         }
-        if (elToAdd) {
-            if (elToAdd['_a'])
-                continue; //already added
-            elToAdd['_a'] = true;
-            goodFakeLinkEls.push(elToAdd);
-        }
-    }
-    //Now clone fake els to real preload links to allow browser to preload link
-    goodFakeLinkEls.forEach((el) => {
-        const href = el.getAttribute('href');
-        el.dataset.tags.split(',').forEach(tag => {
-            let modifiedHref = href;
-            let counter = 0;
-            tag.split('-').forEach(token => {
-                modifiedHref = replace(modifiedHref, '\\{' + counter + '\\}', token);
-                counter++;
+        //Now clone fake els to real preload links to allow browser to preload link
+        goodFakeLinkEls.forEach((el) => {
+            const href = el.getAttribute('href');
+            el.dataset.tags.split(',').forEach(tag => {
+                let modifiedHref = href;
+                let counter = 0;
+                tag.split('-').forEach(token => {
+                    modifiedHref = replace(modifiedHref, '\\{' + counter + '\\}', token);
+                    counter++;
+                });
+                const d = el.dataset;
+                const base = d.baseRef ? window[d.baseRef].href : '';
+                modifiedHref = base + modifiedHref;
+                //from https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content
+                const preloadLink = el.cloneNode();
+                preloadLink.removeAttribute('rel-ish');
+                preloadLink.removeAttribute('data-tags');
+                preloadLink.href = modifiedHref;
+                preloadLink.id = tag.split('-').join('_');
+                preloadLink.rel = el.getAttribute('rel-ish'); //el.getAttribute('rel-ish') if support prefetch
+                document.head.appendChild(preloadLink);
             });
-            const d = el.dataset;
-            const base = d.baseRef ? window[d.baseRef].href : '';
-            modifiedHref = base + modifiedHref;
-            //from https://developer.mozilla.org/en-US/docs/Web/HTML/Preloading_content
-            const preloadLink = el.cloneNode();
-            preloadLink.removeAttribute('rel-ish');
-            preloadLink.removeAttribute('data-tags');
-            preloadLink.href = modifiedHref;
-            preloadLink.id = tag.split('-').join('_');
-            preloadLink.rel = el.getAttribute('rel-ish'); //el.getAttribute('rel-ish') if support prefetch
-            document.head.appendChild(preloadLink);
         });
-    });
-    const notFound = [];
-    for (var key in XtalSip._notFound) {
-        notFound.push(key);
+        const notFound = [];
+        for (var key in XtalSip._notFound) {
+            notFound.push(key);
+        }
+        XtalSip._notFound = {};
+        XtalSip.load(...notFound);
     }
-    XtalSip._notFound = {};
-    XtalSip.load(...notFound);
-} while ('');
+})();
 //# sourceMappingURL=xtal-sip-plus.js.map
