67fc5656ceed307cd1282ce6c59a508be2236a28
bahrus <anderson.bruce.b@gmail.com>
2019-07-17 02:53:39 -0400

    ...

--- xtal-sip.js
+++ xtal-sip.js
@@ -1,14 +1,12 @@
+//import { CSSListener } from './CSSListener.js';
 export function getHost(el) {
     let parent = el;
     while (parent = (parent.parentNode)) {
         if (parent.nodeType === 11) {
-            return parent['host'];
-        }
-        else if (parent.tagName.indexOf('-') > -1) {
             return parent;
         }
         else if (parent.tagName === 'BODY') {
-            return null;
+            return parent;
         }
     }
     return null;
--- xtal-sip.ts
+++ xtal-sip.ts
@@ -1,14 +1,12 @@
-import { CSSListener } from './CSSListener.js';
+//import { CSSListener } from './CSSListener.js';
 
 export function getHost(el: HTMLElement) : HTMLElement | null {
   let parent : any = el;
   while (parent = (parent.parentNode)) {
       if (parent.nodeType === 11) {
-          return (<any>parent)['host'] as HTMLElement;
-      } else if ((<HTMLElement>parent).tagName.indexOf('-') > -1) {
           return parent;
       }  else if (parent.tagName === 'BODY') {
-          return null;
+          return parent;
       }
   }
   return null;
