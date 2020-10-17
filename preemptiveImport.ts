9facff872b21723c34dd5322c4b4512825c29ae5
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-10-17 12:33:28 -0400

    ...

--- preemptiveImport.ts
+++ preemptiveImport.ts
@@ -6,18 +6,32 @@ export function preemptiveImport(arg: PreemptiveLoadingTuple){
         if(linkTag === undefined){
             //TODO:  add wait for xtal-sip tag.
         }else{
-            if(linkTag.dataset.loaded === 'true'){
+            if(linkTag.localName === 'link'){ //security precaution
+                if(linkTag.dataset.loaded === 'true'){
+                    return;
+                }
+                //TODO:  non modules and style links
+                const scriptTag = document.createElement('script');
+                scriptTag.type = 'module';
+                scriptTag.integrity = linkTag.integrity;
+                scriptTag.src = linkTag.href;
+                document.head.appendChild(scriptTag);
+                linkTag.dataset.loaded = 'true';
                 return;
             }
-            //TODO:  non modules and style links
-            const scriptTag = document.createElement('script');
-            scriptTag.type = 'module';
-            scriptTag.integrity = linkTag.integrity;
-            scriptTag.src = linkTag.href;
-            document.head.appendChild(scriptTag);
-            linkTag.dataset.loaded = 'true';
-            return;
         }
     }
-    
+    //No go for linkTag, try importmap.
+    const dynamicImport = arg[1];
+    if(dynamicImport !== undefined){
+        try{
+            dynamicImport();
+            return;
+        }catch(e){}
+    }
+    //No luck with dynamic import, try CDN
+    const CDNPath = arg[2];
+    if(CDNPath !== undefined){
+        import(CDNPath);
+    }
 }
